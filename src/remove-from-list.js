7981ffdad2309768c2e3a521fdad9fb8a2921889
Oromashko <66132151+Oromashko@users.noreply.github.com>
2022-05-02 11:57:48 +0300

    Remove from list completed

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,11 +22,80 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ 
+ function convertArrayToList(arr) {
+  return arr.reverse().reduce((acc, cur) => {
+    if (acc) {
+      const node = new ListNode(cur);
+      node.next = acc;
+      return node;
+    }
+
+    return new ListNode(cur);
+  }, null);
+} 
+class ListNode {
+  constructor(x) {
+    this.value = x;
+    this.next = null;
+  };
+
+
+    print() {
+      let current = this;
+      while(current) {
+        console.log("Node: ", current.value);
+        current = current.next;
+        
+        }
+    }
+};
+
+function removeKFromList(l, k) {
+  
+     if (l != null){
+    let last = 0;
+    let head=l; //  указатель на начало списка
+    let current = l; // указатель на текущий элемент в списке 
+    let prev = null; // предыдущий элемент
+    let num = 0; // текущий номер элемента в списке
+    while (last === 0){
+      /*head.print();*/
+      /*console.log('current value', current.value);*/
+      if (current.value === k){
+        if (prev === null) { // первый элемент
+          head = current.next;
+          current = current.next; 
+          
+        }else { // не первый элемент
+              if (current.next != null) // не первый элемент  и не последний 
+              { 
+                prev.next = current.next; 
+                current=current.next;
+              }
+              else // последний элемент
+              {
+                  prev.next=null;  
+                  last = 1;
+              }
+            
+              }
+    } else // не равен k 
+      {
+        
+        prev = current;  
+        current = current.next;  
+        if (current === null) {last = 1;}
+      }
+    
+  }
+  
+  return head;
+}else {throw new TypeError('Array is Empty'); }
 }
 
-module.exports = {
+     module.exports = {
   removeKFromList
 };
+
+
