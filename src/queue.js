2ad85b1b0513bb650a9192c3ae81d3e279079ab1
Oromashko <66132151+Oromashko@users.noreply.github.com>
2022-05-02 16:51:17 +0300

    Queue completed

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
+/*const { assert } = require('chai');*/
+const { ListNode } = require('../extensions/list-node.js');
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -13,24 +15,58 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+ 
 class Queue {
-
+  constructor() {
+    this.listNode = new ListNode(null); 
+  }
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.listNode;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    let newList;
+    if (this.listNode.value === null) {
+      /*console.log('First element');*/
+      this.listNode.value = value;
+    } else {
+      /*console.log('New element');*/
+      let current = this.listNode;   
+      let prev;   
+      while (current) {
+        prev = current;
+        current = current.next;
+
+      };
+
+        prev.next = new ListNode(value);
+        
+    };
+
+
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.listNode){
+    let element = this.listNode.value;
+    this.listNode = this.listNode.next;
+    return element;
+    }
   }
 }
 
 module.exports = {
   Queue
 };
+
+/*const queue = new Queue();
+queue.enqueue(5);
+queue.enqueue(6);
+queue.enqueue(7);*/
+/*console.log('Dequeue:',queue.dequeue());
+console.log('Dequeue:',queue.dequeue());
+console.log('Dequeue:',queue.dequeue());
+
+console.log(queue);*/
+/*assert.strictEqual(queue.dequeue(), 5);
+assert.strictEqual(queue.dequeue(), 6);*/
